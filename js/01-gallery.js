9a17ae18b10ac8c32e7f1ceef1581f78149ba8ec
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-09-10 12:35:11 +0300

    done

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const ulGalleryRef = document.querySelector("ul.gallery");
+ulGalleryRef.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+ulGalleryRef.addEventListener("click", onClickHandler);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+let instance = null;
+
+function onClickHandler(event) {
+  event.preventDefault();
+  window.addEventListener("keydown", onKeydownHandler);
+  if (event.currentTarget === event.target) {
+    return;
+  }
+  const origUrl = event.target.dataset.source;
+  const description = event.target.alt;
+  instance = basicLightbox.create(
+    `<img src="${origUrl}" alt="${description}" />`
+  );
+  instance.show();
+}
+
+function onKeydownHandler(event) {
+  if (event.key === "Escape") {
+    instance.close();
+  }
+
+  window.removeEventListener("keydown", onKeydownHandler);
+}
